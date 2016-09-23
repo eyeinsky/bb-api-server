b7c20ad92bf5f4ded9d6df1e4bb40bfc7e52d831
eyeinsky <eyeinsky9@gmail.com>
2016-09-23 22:30:04 +0200

    Make app look better

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -4,21 +4,45 @@ angular.module('starter.controllers', [])
 
   var baseUrl = 'http://bb:8080'
 
+  $scope.nowPlaying = 'off'
   $scope.channels = [
       {name: 'P1', suffix: 'p1'}
     , {name: 'Kuku', suffix: 'kuku'}
     , {name: 'Raadio 2', suffix: 'r2'}
+    , {name: 'Bandit Rock', suffix: 'brock'}
   ]
-  $scope.selectChannel = function (c) {
-    console.log('selectChannel', c)
-    $http({
-      method: 'GET',
-      url: baseUrl + '/' + c.suffix
-    }).then(function (response) {
-      console.log('success', response)
-    }, function (response) {
-      console.log('error', response)
+
+  var setPlaying = function (name) {
+    console.log('setPlaying', name)
+    $scope.channels.map(function (c) {
+      console.log('c is', c)
+      if (c.suffix === name) {
+        c.playing = true
+      } else {
+        c.playing = false
+      }
     })
   }
-  console.log('cl')
+
+  $scope.selectChannel = function (c) {
+    console.log('selectChannel', c)
+    $http.get(baseUrl + '/' + c.suffix).then(function (response) {
+      setPlaying(response.data)
+      console.log('success', response)
+    }).catch(function (err) {
+      setPlaying(undefined)
+      console.log('error', err)
+    })
+  }
+
+  $scope.getNowPlaying = function () {
+    $http.get(baseUrl + '/playing').then(function (response) {
+      console.log('success', response)
+      setPlaying(response.data)
+    }).catch(function (err) {
+      console.log('error', err)
+    })
+  }
+
+  $scope.getNowPlaying()
 })
